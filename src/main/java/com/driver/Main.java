ed8b21b4089656a9060116242c118ad823c1cadc
Sanket Garud <sanketgarud10@gmail.com>
2023-04-12 18:01:34 +0530

    assignment 2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,29 @@
 package com.driver;
 
+class Product{
+    public int product(int x, int y) {
+        return x * y;
+    }
+    public int product(int x, int y, int z) {
+        return x * y * z;
+    }
+    public double product(double x, double y) {
+        return x * y;
+    }
+}
 public class Main {
+
+    public static void main(String args[]){
+        int x = 10;
+        int y = 12;
+        int z = 20;
+        double a = 10;
+        double b = 20;
+
+        Product p = new Product();
+        p.product(x, y);
+        p.product(x, y, z);
+        p.product(a, b);
+    }
 
 }
